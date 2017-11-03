16392bc7eb900a80dcda921809bb9b29c8c08485
Axel Rantil <axelrantil@Axels-MacBook-Pro.local>
2017-11-03 09:01:35 +0100

    lab1

--- lab1/assignment1.R
+++ lab1/assignment1.R
@@ -1,16 +1,26 @@
 knearest=function(data,k,newdata) {
   
-  n1=dim(data)[1]
-  n2=dim(newdata)[1]
-  p=dim(data)[2]
-  Prob=numeric(n2)
-  X=as.matrix(data[,-p])
-  Xn=as.matrix(newdata[-p])
-  X=X/matrix(sqrt(rowSums(X^2)), nrow=n1, ncol=p-1)
-
-#MISSING: implement steps ii)-iv)
+  n1=dim(data)[1] ## Number of training
+  n2=dim(newdata)[1] ## Number of test
+  p=dim(data)[2] ## Number of columns
+  Prob=numeric(n2) ## 0 vector of length n2 (number of test)
+  X=as.matrix(data[,-p]) ## X
+  Xn=as.matrix(newdata[-p]) ## Y
+  X=X/matrix(sqrt(rowSums(X^2)), nrow=n1, ncol=p-1) #Normalized
+  
+  Xn=Xn/matrix(sqrt(rowSums(Xn^2)), nrow=n1, ncol=p-1) #Normalized
+  
+  C = X%*%t(Xn)
+  
+  D = matrix(1, dim(C)[1], dim(C)[2]) - C
   
   for (i in 1:n2 ){
+    kNN <- which.min(D[,i])
+    for (j in 2:k){
+      kNN <- c(kNN, which.min(D[-kNN,i]))
+    }
+    Prob[i] <- mode(X[kNN, ncol])
+    
 #MISSING: use the computed distance matrix to find 
     #which observations are the nearest neighbors to case #i
 #MISSING: derive probability value 'Prob[i]' by using the
@@ -18,8 +28,6 @@ knearest=function(data,k,newdata) {
   }
   return(Prob)
 }
-
-
 
 ROC=function(Y, Yfit, p){
   m=length(p)
@@ -33,3 +41,14 @@ ROC=function(Y, Yfit, p){
   return (list(TPR=TPR,FPR=FPR))
 }
 
+data = read.csv("spambase.csv", header = TRUE, sep=";")
+
+data[,ncol(data)] = as.factor(data[,ncol(data)])
+
+n=dim(data)[1]
+set.seed(12345)
+id=sample(1:n, floor(n*0.5))
+train=data[id,]
+test=data[-id,]
+
+knearest(train, k = 5, test)
