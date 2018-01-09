0647ad33c165b23530bb0db0e07aa91daa4e055c
Axel Rantil <axelranti@gmail.com>
2018-01-09 08:56:10 +0100

    na

--- exam_exercises/tenta1/assignment1.R
+++ exam_exercises/tenta1/assignment1.R
@@ -1,2 +1,18 @@
 library(readr)
 crx <- read_csv("~/Projects/iml/exam_exercises/tenta1/crx.csv")
+
+n=dim(crx)[1]
+set.seed(12345)
+id=sample(1:n, floor(n*0.8))
+train=crx[id,]
+test=crx[-id,]
+
+library(tree)
+treefit <- tree(Class~.,data=train)
+plot(treefit)
+text(treefit)
+
+train = train[-2,]
+treefit <- tree(Class~.,data=train)
+plot(treefit)
+text(treefit)
